72ce5c2f0cb61acf977801a2ec43f13e51c38811
david-olarte-velasco-13 <dagov1998@gmail.com>
2025-12-28 18:40:20 -0500

    fix: clean legacy files

--- start-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script para iniciar el servidor FastAPI
-# Uso: ./start-server.sh
-
-cd "$(dirname "$0")/backend"
-source venv/bin/activate
-uvicorn main:app --reload --host 0.0.0.0 --port 8000
